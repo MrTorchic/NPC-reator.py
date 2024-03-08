66c67cc685a221d24ab605f81e48fac8d82acef4
Kimberbach Industries <67416737+MrTorchic@users.noreply.github.com>
2024-03-08 00:04:51 -0800

    Release v1.0.2
    condensed code and change GUI style

--- main.py
+++ main.py
@@ -45,70 +45,26 @@ namePath = 'Lists/Names/'
 jobFiles = [f for f in listdir(jobPath) if isfile(join(jobPath, f))]
 nameFiles = [f for f in listdir(namePath) if isfile(join(namePath, f))]
 #File Lists
-def randName(name, quantity):
+def main():
+	quantity = numVal.get()
+	amount = quantity
 	nameList.clear()
-	print(quantity)
-	with open(f'Lists/Names/{name}') as f:
+	occupationList.clear()
+	with open(f'Lists/Names/{namefileVal.get()}') as f:
 		line_count = 0
-		print(quantity)
 		for line in f:
 			line_count += 1
 		for i in range(0, quantity):
-			#get the contents of the line
-			x=rng.integers(line_count)
-			y=rng.integers(line_count)
-			firstName = linecache.getline(f'Lists/Names/{name}', x)
-			lastName = linecache.getline(f'Lists/Names/{name}', y)
-
-			print(i)
-			nameStr=f'{firstName.rstrip()} {lastName.rstrip()}'
-			#append line
-			nameList.append(f'{nameStr}\n')
-			print(nameList)
-		else:
-			print(f"loop 1 success")
-
-
-
-
-def randJob(jobName, quantity):
-	occupationList.clear()
-	print(quantity)
-	with open(f'Lists/Jobs/{jobName}') as f:
-		line_counta = 0
-		for line in f:
-			line_counta += 1
-		for i in range(0, quantity):
-			#randomly pick a
-			x = rng.integers(line_counta)
-			#get the random job
-			jobNamea = linecache.getline(f'Lists/Jobs/{jobName}', x)
-
-			#append randJob to $occupationList
-			occupationList.append(f'Occupation: {jobNamea}')
-		else:
-			#confirms when process is finished
-			print('loop 2 success')
-
-
-def merge(x):
-	mergedList.clear()
-	for i in range(0,x):
-		print(i)
-		mergedList.append(f'{nameList[i].rstrip()} - {occupationList[i]}')
-
-def main():
-	#make sure the num is reset
-	#clear output field
-
-	#rand seed
-	#set the amout variable
-	amount=numVal.get()
-	if jobStatus.get()==0: #Jobs Disabled
-		num=0
+			rand1=rng.integers(line_count)
+			rand2=rng.integers(line_count)
+			firstName = linecache.getline(f'Lists/Names/{namefileVal.get()}', rand1)
+			lastName = linecache.getline(f'Lists/Names/{namefileVal.get()}', rand2)
+			nameStr=f'First: {firstName.rstrip()} | Last: {lastName.rstrip()}'+'\n'
+			nameList.append(f'{nameStr}')
+	if jobState.get()==0: #Occupation not enable, make and append nameList
 		numpy.random.seed()
 		output.delete(0.0, tk.END)
-		randName(nameSelect.get(),amount)
+		num=0
 		for i in nameList:
 			output.insert(f'{num}.0',i)
 			for i in nameList:
@@ -116,27 +72,23 @@ def main():
 					num+=1
 				else:
 					num=0
-	elif jobStatus.get()==1: #Jobs Enabled
+	with open(f'Lists/Jobs/{jobfileVal.get()}') as g: #select randomJob
+		line_counta = 0
+		for line in g:
+			line_counta += 1
+		for i in range(0, quantity):
+			rand3 = rng.integers(line_counta)
+			randomJob = linecache.getline(f'Lists/Jobs/{jobfileVal.get()}', rand3)
+			occupationList.append(f'Occupation: {randomJob}')
+	if jobState.get()==1: #Merge
 		num=0
 		numpy.random.seed()
 		output.delete(0.0, tk.END)
-		randName(nameSelect.get(),amount)
-		randJob(jobSelect.get(),amount)
-		merge(amount)
-		for i in mergedList:
-			a = f'{num}.0'
-			b = f'{i}'
-			output.insert(a,b)
-			for i in mergedList:
-				if num<amount:
-					num+=1
-				else:
-					num=0
-		else:
-			print('main loop finished')
-			print('1')
-
-
+		mergedList.clear()
+		for ii in range(0, quantity):
+			print(mergedList)
+			mergedList.append(f'{nameList[ii].rstrip()} | {occupationList[ii].rstrip()}')
+			output.insert(f'{ii}.0',f'{mergedList[ii]}\n')
 
 #Init Window
 window = tk.Tk()
@@ -144,20 +96,22 @@ ttk.Style().theme_use('clam')
 window.configure(bg='#9932cc')
 window.title('NPC-reator')
 #Name
-nameSelect=tk.StringVar()
-nameSelect.set('N/A')
-nameEntry=tk.OptionMenu(window, nameSelect,*nameFiles)
-nameEntry.configure(background="#dda0dd")
-nameEntry.grid(row=0,column=8)
+namefileVal=tk.StringVar()
+namefileVal.set('devNames.txt')
+nameMenu=tk.OptionMenu(window, namefileVal,*nameFiles)
+nameMenu.configure(background="#dda0dd")
+nameMenu.grid(row=0,column=8)
 nameLabel=tk.Label(text='Name List:',bg='#dda0dd').grid(row=0,column=6)
 #Job
-jobSelect = tk.StringVar()
-jobSelect.set('N/A')
-jobEntry = tk.OptionMenu(window, jobSelect,*jobFiles)
-jobEntry.configure(background="#dda0dd")
-jobEntry.grid(row=0,column=10)
-jobStatus= tk.IntVar()
-jobToggle= tk.Checkbutton(text='Job List:',variable=jobStatus,bg='#dda0dd').grid(row=0,column=9)
+jobfileVal = tk.StringVar()
+jobfileVal.set('devOccupations.txt')
+#
+jobMenu = tk.OptionMenu(window, jobfileVal,*jobFiles)
+jobMenu.configure(background="#dda0dd")
+jobMenu.grid(row=0,column=10)
+#
+jobState= tk.IntVar()
+jobToggle= tk.Checkbutton(text='Job List:',variable=jobState,bg='#dda0dd').grid(row=0,column=9)
 #Num
 numLabel = tk.Label(window,text='Quantity:',bg='#dda0dd').grid(row=0,column=11)
 numVal = tk.IntVar()
@@ -167,7 +121,7 @@ numEntry.configure(background="#dda0dd")
 numEntry.grid(row=0,column=12)
 #Other
 start = tk.Button(window, text = 'Run', bg='#dda0dd', command=main).grid(row=0,column=3)
-output=tk.Text(height=25,width=50, bg='#e6e6fa')
+output=tk.Text(height=25,width=75, bg='#dda0dd')
 output.grid(row=0,column=1)
 label = tk.Label(text='NPC-reator',bg='#dda0dd').grid(row=0,column=0)
 window.mainloop()
